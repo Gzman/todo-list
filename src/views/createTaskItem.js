76e90c3d4e16a21da230b06d96aff2dbac98447e
Gzman <ganzmann@protonmail.com>
2021-06-18 01:57:57 +0200

    Implemented project view

---
+++ src/views/createTaskItem.js
@@ -0,0 +1,54 @@
+import { ViewEvents, ViewMediator } from "../mediator/viewMediator.js"
+
+function createTaskItem(projectTitle, { title, description, priority, dueDate, isComplete }) {
+    const $taskItem = document.createElement("div");
+    $taskItem.dataset.project = projectTitle;
+    $taskItem.dataset.task = title;
+
+    const $inputComplete = document.createElement("input");
+    $inputComplete.type = "checkbox";
+    $inputComplete.classList.add("task-item-complete");
+    $inputComplete.checked = isComplete;
+
+    const $taskName = document.createElement("p");
+    $taskName.classList.add("task-item-name");
+    $taskName.textContent = title;
+
+    const $taskCtrls = document.createElement("div");
+    $taskCtrls.classList.add("task-ctrls");
+    const $taskRemoveBtn = document.createElement("button");
+    $taskEditBtn.classList.add("task-item-edit");
+    const $taskEditBtn = document.createElement("button");
+    $taskRemoveBtn.classList.add("task-item-delete");
+    $taskEditBtn.addEventListener("click", () => {
+        document.querySelector(".edit-task-container").classList.add("showItem");
+        ViewMediator.publish(ViewEvents.EDIT_TASK_CLICKED, title);
+    });
+    $taskRemoveBtn.addEventListener("click", () => {
+        ViewMediator.publish(ViewEvents.REMOVE_TASK, projectTitle, title);
+        $taskItem.remove();
+    });
+    $taskCtrls.append($taskEditBtn, $taskRemoveBtn);
+
+    $taskItem.classList.add(`priority-${priority.toLowerCase()}`);
+
+    const $taskDate = document.createElement("input");
+    $taskDate.type = "date";
+    $taskDate.readOnly = true;
+    $taskDate.value = dueDate;
+
+    $taskItem.append($inputComplete, $taskName, $taskCtrls, $taskDate);
+    return $taskItem;
+}
+
+/*function createExtendView(description) {
+    const $div = document.createElement("div");
+    const $textArea = document.createElement("textarea");
+    $textArea.readOnly = true;
+    $textArea.style.display = "none";
+    $textArea.rows = 5;
+    $textArea.value = description;
+    return $textArea;
+}*/
+
+export { createTaskItem };
--- src/views/projectListView.js
+++ src/views/projectListView.js
@@ -1,28 +1,26 @@
-import {ViewEvents, viewMediator} from "../mediator/viewMediator.js"
+import {ViewEvents, ViewMediator} from "../mediator/viewMediator.js"
 
-(function projectFilter() {
+(function ProjectFilter() {
     const $filterBtns = document.querySelectorAll(".projects-filter button");
     $filterBtns.forEach((filterBtns) => {
         filterBtns.addEventListener("click", (event) => {
             const title = event.currentTarget.textContent;
-            viewMediator.publish(ViewEvents.SELECT_PROJECT, title);
+            ViewMediator.publish(ViewEvents.PROJECT_SELECTED, title);
         });
     });
 })();
 
-(function projectsUserCreated() {
+(function ProjectsUserCreated() {
     const $projectItems = document.querySelector(".projects-items");
     const $projectAddBtn = document.querySelector(".projects-add-btn");
     const $newProjectLightbox = document.querySelector(".new-project-lightbox");
     let projects = new Set();
 
-    viewMediator.subscribe(ViewEvents.CREATE_PROJECT, (title) => {
-        console.log("Project created: ", title);
+    ViewMediator.subscribe(ViewEvents.CREATE_PROJECT, (title) => {
         projects.add(title);
     });
 
-    viewMediator.subscribe(ViewEvents.REMOVE_PROJECT, (title) => {
-        console.log("Project removed: ", title);
+    ViewMediator.subscribe(ViewEvents.REMOVE_PROJECT, (title) => {
         projects.remove(title);
     });
 
@@ -30,8 +28,7 @@ import {ViewEvents, viewMediator} from "../mediator/viewMediator.js"
         const project = event.target.closest(".project-item");
         if(!project) {
             const title = project.data.title;
-            console.log("Project selected: ", title);
-            viewMediator.publish(ViewEvents.SELECT_PROJECT, title);
+            ViewMediator.publish(ViewEvents.PROJECT_SELECTED, title);
         }
     });
 
